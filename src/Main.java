84672f04c1b7b44ea207d04a34ee5f887887f1c9
lchrzanowski <lukasz.chrzanowski2@gmail.com>
2021-09-21 18:02:15 +0200

    1

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main(String[] args) {
+        /* Przykłady wyświetlania tekstu w konsoli */
+        System.out.println("Hello World!");
+        System.out.println("First program in Java.");
+
+        System.out.printf("Anything %s here for an %s. \n","passed", "example");
+        System.out.printf("Like random number %d. \n", 5);
+
+        System.out.print("These are just");
+        System.out.print(" some examples");
+        System.out.print(" of strings.");
+    }
+}
